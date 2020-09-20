6615da00fa60c1c69df47d625439b8107b7edea9
13879303480 <2287061933@qq.com>
2020-09-20 16:16:17 +0800

    Initial commit

---
+++ src/main/java/Chapter1/Stream_method.java
@@ -0,0 +1,14 @@
+package Chapter1;
+
+/**
+ * @Classname Stream_mothed
+ * @Description TODO
+ * @Date 2020/9/20 16:02
+ * @Created by Jieqiyue
+ */
+public class Stream_method {
+
+    public static void main(String[] args) {
+
+    }
+}
