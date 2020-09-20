f74aabf33439932f44062756327da2f4a5607020
13879303480 <2287061933@qq.com>
2020-09-20 16:37:53 +0800

    1

--- src/main/java/Chapter1/Stream_method.java
+++ src/main/java/Chapter1/Stream_method.java
@@ -1,14 +1,36 @@
 package Chapter1;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 /**
  * @Classname Stream_mothed
- * @Description TODO
+ * @Description 本类就是介绍了一些stream的方法。
  * @Date 2020/9/20 16:02
  * @Created by Jieqiyue
  */
 public class Stream_method {
 
     public static void main(String[] args) {
+        // map方法可以接收一个Function.所以可以对stream中的每一个元素进行改变。
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8);
+        List<Integer> collect = list.stream().map(e -> e * 2).collect(Collectors.toList());
+        System.out.println(collect);
 
+        // filter 方法接收一个Predicate的函数式接口。
+        List<Integer> collect1 = list.stream().filter(e -> e % 2 == 0).collect(Collectors.toList());
+        System.out.println(collect1);
+
+        // flatmap (扁平化)
+        String[] words = {"hello","kitty"};
+
+        Stream<String[]> stream = Arrays.stream(words).map(w -> w.split(""));
+
+        Stream<String> stringStream = stream.flatMap(Arrays::stream);
+
+        stringStream.distinct().forEach(System.out::println);
     }
 }
