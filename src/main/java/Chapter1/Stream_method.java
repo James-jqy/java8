d74d8189e0fbb2c648f764201f8323435961f991
13879303480 <2287061933@qq.com>
2020-09-20 17:09:56 +0800

    1

--- src/main/java/Chapter1/Stream_method.java
+++ src/main/java/Chapter1/Stream_method.java
@@ -3,6 +3,7 @@ package Chapter1;
 import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -32,5 +33,18 @@ public class Stream_method {
         Stream<String> stringStream = stream.flatMap(Arrays::stream);
 
         stringStream.distinct().forEach(System.out::println);
+
+        // match 方法，返回的是bool值
+        List<Integer> list2 = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8);
+        boolean b = list2.stream().allMatch(e -> e > 0);
+        System.out.println(b);
+
+        // find 方法,optional 详细可以看它的api。
+        Stream<Integer> stream1 = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8).stream();
+        // 要注意这个地方，它返回的是一个optional。原来的stream已经被中断了。
+        Optional<Integer> any = stream1.filter(e->e>7).findAny();
+        Integer integer = any.get();
+        System.out.println(integer);
+
     }
 }
